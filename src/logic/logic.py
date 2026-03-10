b500024e9eb54ecc79a99639e8a06172fa69331a
SaRodriguez285 <santiago.pulidoa22@gmail.com>
2026-03-10 02:35:54 -0500

    Corrige implementación de operaciones lógicas asegurando retorno booleano

--- src/logic/logic.py
+++ src/logic/logic.py
@@ -1,35 +1,28 @@
 class Logica:
-    
+
     def AND(self, a, b):
-        return a and b
+        return bool(a and b)
 
-    
     def OR(self, a, b):
-        return a or b
-    
+        return bool(a or b)
+
     def NOT(self, a):
-        return not a
-    
+        return bool(not a)
+
     def XOR(self, a, b):
-        return a != b
-    
+        return bool(a) != bool(b)
+
     def NAND(self, a, b):
-        return not (a and b)
+        return not (bool(a) and bool(b))
 
-    
     def NOR(self, a, b):
-        return not (a or b)
+        return not (bool(a) or bool(b))
 
-    
     def XNOR(self, a, b):
-        return a == b
+        return bool(a) == bool(b)
 
     def implicacion(self, a, b):
-        return (not a) or b
+        return (not bool(a)) or bool(b)
 
-    
     def bi_implicacion(self, a, b):
-        return a == b
-
-    
-    
+        return bool(a) == bool(b)
