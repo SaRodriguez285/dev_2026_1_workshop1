38d57eabf30db9acc948c5a7f4a1a8d199d5a4ba
SaRodriguez285 <santiago.pulidoa22@gmail.com>
2026-03-10 02:53:55 -0500

    Implementa generación de matriz identidad

--- src/matrix/matrix.py
+++ src/matrix/matrix.py
@@ -75,21 +75,8 @@ class Matrix:
 
 
     def identidad(self, n):
-        """
-        Genera una matriz identidad de tamaño n x n.
-        La diagonal principal tiene 1s y el resto 0s.
+        return [[1 if i == j else 0 for j in range(n)] for i in range(n)]
 
-        Args:
-            n (int): Tamaño de la matriz identidad
-
-        Returns:
-            list: Matriz identidad n x n
-
-        Ejemplo:
-            identidad(2) -> [[1, 0], [0, 1]]
-            identidad(3) -> [[1, 0, 0], [0, 1, 0], [0, 0, 1]]
-        """
-        pass
 
     def diagonal(self, matriz):
         """
